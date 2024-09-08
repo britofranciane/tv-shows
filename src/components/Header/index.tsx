df9511b7fa0012ece421ff3d055317b9758cf27b
franciane-brito <francianebrito99@gmail.com>
2024-09-08 15:01:48 -0300

    feat: add List Shows

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
+import { Link, useLocation } from 'react-router-dom';
 import './styles.scss';
 
 const Header = () => {
+  const location = useLocation();
+  const isNotHomePage = location.pathname !== '/';
   return (
     <div className="header">
       <h2 className="header__title">TV Shows</h2>
-      {/* <Link to="/" className="header__link">
-        Back to home page
-      </Link> */}
+      {isNotHomePage && (
+        <Link to="/" className="header__link">
+          List Shows
+        </Link>
+      )}
     </div>
   );
 };
