1c120e8cf5797ff3d527360ae2484794463c92f4
franciane-brito <francianebrito99@gmail.com>
2024-09-08 10:39:47 -0300

    feat: add class container-loader

--- src/pages/Details/components/EpisodeList/EpisodeListLoader.tsx
+++ src/pages/Details/components/EpisodeList/EpisodeListLoader.tsx
@@ -5,10 +5,10 @@ const EpisodeListLoader: React.FC = () => {
   return (
     <>
       {Array.from({ length: 6 }).map((_, index) => (
-        <React.Fragment key={index}>
+        <div className="episodes-section__container-loader" key={index}>
           <SkeletonLoader type={SkeletonType.Image} height="200px" />
           <SkeletonLoader type={SkeletonType.Text} height="1rem" />
-        </React.Fragment>
+        </div>
       ))}
     </>
   );
